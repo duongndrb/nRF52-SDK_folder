f874492e1dd26351b3d759b2151b4b1b17a87cf1
Nguyễn Đình Dương <47983821+duongndrb@users.noreply.github.com>
2021-12-20 11:21:20 +0700

    Create code_button_1led.c

---
+++ code_button_1led.c
@@ -0,0 +1,37 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+#define LED 17 
+#define Button 13
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+
+    nrf_gpio_cfg_output(LED); // Configures the Led pin as Output pin
+    
+    nrf_gpio_cfg_input(Button, NRF_GPIO_PIN_PULLUP); // Configure the Button pin as Input pin
+
+    nrf_gpio_pin_set(LED); //Turns off the LED
+
+    /* Toggle LEDs. */
+    while (true)
+    {
+      if(nrf_gpio_pin_read(Button) == 0)
+      {
+        nrf_gpio_pin_clear(LED); //Turn on the LED
+
+        while(nrf_gpio_pin_read(Button) == 0); //Stay in this loop until the button is released
+
+        nrf_gpio_pin_set(LED); //Turns off the LED
+      }
+    }
+}
+
+/**
+ *@}
+ **/
